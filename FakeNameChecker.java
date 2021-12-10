a931309bfe75ab26a8115cb55a97965481cc12fc
eduardogrilo <jose_egrl@hotmail.com>
2021-12-10 22:41:34 +0000

    Debug does not work. It is just saving pushong this file to test.

--- FakeNameChecker.java
+++ FakeNameChecker.java
@@ -1,9 +1,29 @@
 public class FakeNameChecker {
-    public boolean isSuspicious(String nameA, String nameB) {
-        return false;
-    }
-}
 
-class Main{
+    public boolean isSuspicious(String nameA, String nameB) {
+        
+        int count_charA = 0, count_charB = 0;
+
+        for(int reference = 0; reference < nameA.length()-1; reference++){
+            for(int indexA = reference+1; indexA < nameA.length(); indexA++){
+                if(nameA.charAt(reference) == nameA.charAt(indexA)){
+                    count_charA++;
+                }
+            }
+            for(int indexB = 0; indexB < nameB.length(); indexB++){
+                if(nameA.charAt(reference) == nameB.charAt(indexB)){
+                    count_charB++;
+                }
+            if(count_charA != count_charB){
+                return false;
+            }
+        }        
+    }
+    return true;
+}
+    public void main(String[] args) {
+        System.out.println(isSuspicious("Orlando","Ronaldo"));
+    }
+
 
 }
